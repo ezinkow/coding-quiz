c4534ed0157c343bdd3598bb6879fa8b65c70294
Elan Zinkow <ezinkow@gmail.com>
2020-10-19 17:27:23 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var theQuiz = document.querySelector("#quiz")
+var eachQuestion = document.querySelector("#question")
+
+
+var Question1 = {
+    wrong1 : "wrong answer 1"
+    wrong2 : "wrong answer 2"
+    wrong3 : "wrong answer 3"
+    right : "right answer"
+};
+
+var Question2 = {
+    wrong1 : "wrong answer 1"
+    wrong2 : "wrong answer 2"
+    wrong3 : "wrong answer 3"
+    right : "right answer"
+};
+
+var Question3 = {
+    wrong1 : "wrong answer 1"
+    wrong2 : "wrong answer 2"
+    wrong3 : "wrong answer 3"
+    right : "right answer"
+};
